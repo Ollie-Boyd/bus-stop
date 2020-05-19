8ed3fb403e38ede929dbcda269dc0fd2d361c363
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-05-19 14:16:06 +0100

    setup complete

---
+++ spec/bus_stop_lap_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../bus')
+require_relative('../person')
+
+class BusStopTest < Minitest::Test
+
+    def setup
+
+    end
+
+end
